e7fa31f246eb3d87e87b61e75b14fcc5673ae60f
Joey Baker <joey@byjoeybaker.com>
2014-08-17 18:36:49 -0700

    smarter port finding

--- start.sh
+++ start.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
+# unoffical bash strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
 
-# copy the config so we don't overrite the orig
-cp -f /data/bud.json $(hostname)_bud.json
-# replace any refrence to backend_ip so we can link containers
-sed -e "s/backend_ip/$(cat /etc/hosts | grep backend | awk '{print $1}')/" -i /data/$(hostname)_bud.json
+function start_bud(){
+  # Docker sets this var based on linking as "backend"
+  #                               remove the  tcp://
+  local backend="$(echo $BACKEND_PORT | cut -c 7-)"
+  #                             get just th ip
+  local backend_ip="$(echo $backend | cut -d ':' -f1)"
+  local backend_port="$(echo $backend | cut -d ':' -f2-)"
 
-exec bud -c /data/$(hostname)_bud.json
+  # copy the config so we don't overrite the orig
+  cp -f /data/bud.json $(hostname)_bud.json
+  echo "Backend IP curl: "
+  curl "$backend"
+  # replace any refrence to backend_ip so we can link containers
+  echo "Setting backend ip to: $backend_ip"
+  sed -e "s/backend_ip/$backend_ip/" -i /data/$(hostname)_bud.json
+  sed -e "s/backend_port/$backend_port/" -i /data/$(hostname)_bud.json
+  exec bud -c /data/$(hostname)_bud.json
+  # cleanup after ourselves since bud needs a file
+  rm -f /data/$(hostname)_bud.json
+}
+start_bud
